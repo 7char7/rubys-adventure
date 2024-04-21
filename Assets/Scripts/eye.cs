e342b96a73856524382d0203fa30088de9220c9e
7char7 <7charlotte72@gmail.com>
2024-04-21 16:31:09 -0400

    Update eye.cs

--- Assets/Scripts/eye.cs
+++ Assets/Scripts/eye.cs
@@ -8,10 +8,17 @@ public class eye : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        
+        //checks if the robot has been talked to 
+        if (RubyController.talked > 0)
+        {
+        //this sets the amount of eyes up by 1
+        RubyController.eyeCounter += 1;
 
         Destroy(gameObject);
-        Debug.Log("Object that entered the trigger : " + other);
+
+
+        }
+        
 
     }
 }
